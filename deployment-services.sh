94ccdd3a156d53e313dc4ad3cf7849156c537ab8
Rodrigo Pereira <rodrigo.c.pereira@live.com>
2020-12-11 13:10:45 -0400

    [Services] Kubernetes Services

--- deployment-services.sh
+++ deployment-services.sh
@@ -11,34 +11,31 @@ echo "Building docker images..."
 docker build -t account-service ./account
 docker build -t gateway-service ./gateway
 
-### Recreate secret and mongo pods
-echo "Deleting pods..."
-kubectl delete -f kubernetes/mongo/deployment.yaml
-kubectl delete -f kubernetes/mongo/service.yaml
-kubectl delete -f kubernetes/mongo/volume.yaml
-
-kubectl delete -f kubernetes/postgres/deployment.yaml
-kubectl delete -f kubernetes/postgres/service.yaml
-kubectl delete -f kubernetes/postgres/volume.yaml
-
-kubectl delete -f account/kubernetes/deployment.yaml
-kubectl delete -f account/kubernetes/service.yaml
-
-kubectl delete -f gateway/kubernetes/deployment.yaml
-kubectl delete -f gateway/kubernetes/service.yaml
-
 ###
 echo "Creating pods..."
-kubectl create -f kubernetes/mongo/volume.yaml
-kubectl create -f kubernetes/mongo/service.yaml
-kubectl create -f kubernetes/mongo/deployment.yaml
+kubectl create -f kubernetes/persistent-volume.yaml
+kubectl create -f kubernetes/persistent-claim.yaml
 
-kubectl create -f kubernetes/postgres/volume.yaml
-kubectl create -f kubernetes/postgres/service.yaml
-kubectl create -f kubernetes/postgres/deployment.yaml
+kubectl create -f kubernetes/postgres-service.yaml
+kubectl create -f kubernetes/mongo-service.yaml
+kubectl create -f kubernetes/elasticsearch-service.yaml
+kubectl create -f kubernetes/kibana-service.yaml
+kubectl create -f kubernetes/logstash-service.yaml
+
+kubectl create -f kubernetes/logstash-configmap.yaml
+
+kubectl create -f kubernetes/elasticsearch-deployment.yaml
+kubectl create -f kubernetes/kibana-deployment.yaml
+kubectl create -f kubernetes/logstash-deployment.yaml
+kubectl create -f kubernetes/mongo-deployment.yaml
+kubectl create -f kubernetes/postgres-deployment.yaml
 
 kubectl create -f account/kubernetes/service.yaml
 kubectl create -f account/kubernetes/deployment.yaml
 
 kubectl create -f gateway/kubernetes/service.yaml
 kubectl create -f gateway/kubernetes/deployment.yaml
+
+### Unset minikube docker environment
+echo "Unsetting docker environment..."
+eval "$(minikube docker-env -u)"
